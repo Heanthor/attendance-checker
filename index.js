368392375d36942c74347f700b8e4e086897805a
Reed Trevelyan <reedtrevelyan@gmail.com>
2016-08-25 17:27:06 -0400

    Bot tracks user's online status

--- index.js
+++ index.js
@@ -13,5 +13,42 @@ var token = fs.readFile("token.txt", 'utf8', function(err, data) {
 
 // get users
 mybot.on("ready", function() {
-	console.log(mybot.users);
+	var users = mybot.users;
+	var statuses = {
+		online: [],
+		offline: [],
+		idle: []
+	}
+
+	users.forEach(function(user) {
+		var username = user["username"];
+		var status = user["status"];
+
+		if (!user["bot"]) {
+			statuses[status].push(username);
+		}
+	});
+
+	statuses["online"].forEach(function(identity) {
+		console.log(identity + " is\033[1;32m Online\033[0m");
+	});
+
+	statuses["idle"].forEach(function(identity) {
+		console.log(identity + " is\033[1;33m Idle\033[0m");
+	});
+
+	statuses["offline"].forEach(function(identity) {
+		console.log(identity+  " is\033[1;31m Offline\033[0m");
+	});
 });
+
+// function formatStatusString(identity, printString) {
+// 	var username = identity[0];
+// 	var note = identity[1];
+
+// 	if (note == null) {
+// 		return "(Missing IGN note) " + username + " is " + printString;
+// 	} else {
+// 		return note + " is " + printString;
+// 	}
+// }
